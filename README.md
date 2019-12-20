e0f02a352018c672946d8aa46a3c1561430fda78
kaychamo <fb.bonnet@gmail.com>
2019-12-20 15:18:01 +0100

    v0

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # dcss-lua-fab
 some lua scripts as qol for the DCSS game
+
+and much credits to https://github.com/HilariousDeathArtist/DCSSConfigFile, thanks for sharing some code!
+
+IT WORKS :
+* sendkey 'zu' over skeletons, then call 'o' key (autoexplore)
+* sendkey 'ctrl+e' when you got a minion so you run at its max speed while no ennemy are in sight
+
+
+HOWTO : 
+for now, you must manually copy the contents of the lua files to your .rc from the dcss website
+then read the first commented line to bind a macro that runs the function
+
+
+TODO :
+* deal with food (move, butcher...)
+* totally replace 'o' key (autoexplore)
+* handle other animate skill
+* handle yred
+
+
+please contribute, by opening issues for example
---
+++ animate.lua
@@ -0,0 +1,100 @@
+-- To use this create a macro with the following function call in game
+-- ===animate
+auto_butcher = very full
+: function animate()
+:   local is_safe = (first_monster == nil)
+:   local mp, max_mp = you.mp()
+:   local hp, max_hp = you.hp()
+:
+:   local you_are_deep_dwarf = string.find(you.race(), "Deep Dwarf")
+:   local you_are_mummy = string.find(you.race(), "Mummy")
+:
+:   local near_starving = ( string.find(you.hunger_name(), "near starving") )
+:   local starving = ( string.find(you.hunger_name(), "starving") and not string.find(you.hunger_name(), "near") )
+:   local should_rest = not_full(hp, mp, max_hp, max_mp)
+:
+:   local can_cast_regen = known_spells["Regeneration"] and (mp>3) and (spells.fail("Regeneration") < 20)
+:   local you_know_sublimation = known_spells["Sublimation of Blood"] and (spells.fail("Sublimation of Blood") < 20) and (mp>3)
+:   local you_know_animate_skeleton = known_spells["Animate Skeleton"] and (spells.fail("Animate Skeleton") < 20) and (mp>1)
+:   local you_know_animate_dead = known_spells["Animate Dead"] and (spells.fail("Animate Dead") < 20) and (mp>4)
+:
+:   -- if starving and no near corps
+:   if (starving or near_starving) then
+:       sendkeys('o')
+:   end
+:
+:   if should_rest then
+:       crawl.mpr("<green>should rest.</green>")
+:       if can_cast_regen then
+:           crawl.mpr("<green>Autocasting Regen.</green>")
+:           sendkeys('zr')
+:       end
+:       sendkeys('5')
+:   end
+:
+:   if ( on_corpses() and (you_know_sublimation or you_know_animate_skeleton or you_know_animate_dead) ) then
+:       crawl.mpr("<cyan>Autocasting zu</cyan>")
+:       sendkeys('zu')
+:       sendkeys('*e')
+:       if (string.find(crawl.messages(10), escape("You travel at normal speed"))) then
+:           sendkeys('*e')
+:       end
+:   end
+:   sendkeys('o')
+:
+: end
+
+<
+  function not_full(hp, mp, max_hp, max_mp)
+    local you_are_mummy = string.find(you.race(), "Mummy")
+    local you_are_deep_dwarf = string.find(you.race(), "Deep Dwarf")
+    return  ( you.slowed()
+            or you.poisoned()
+            or you.confused()
+            or you.exhausted()
+            or ((hp < max_hp) or (mp < max_mp)) )
+  end
+
+  function on_corpses()
+    local fl = you.floor_items()
+    for it in iter.invent_iterator:new(fl) do
+      if (string.find(it.name(), "corpse") or string.find(it.name(), "skeleton")) then
+          --and not string.find(it.name(), "rotting")
+          --and not string.find(it.name(), "plague") then
+        return true
+      end
+    end
+    return false
+  end
+
+  function floor_items()
+    return iter.invent_iterator:new(you.floor_items())
+  end
+
+  function sendkeys(command)
+    crawl.flush_input()
+    crawl.sendkeys(command)
+--    coroutine.yield(true)
+    crawl.flush_input()
+  end
+
+  --Escapes the special characters in a string for pattern matching
+  function escape(str)
+    --Escapes parens and dash "()-"
+    local escaped = str:gsub('[%(%)%-]','%\%1')
+    --Removes any coloration parts of the string
+    return (escaped:gsub('<[^<]*>',''))
+  end
+
+  local function init_spells()
+    local spell_list = {}
+
+    for _, spell_name in ipairs(you.spells()) do
+      spell_list[spell_name] = true
+    end
+
+    return spell_list
+  end
+
+  known_spells = init_spells()
+>
